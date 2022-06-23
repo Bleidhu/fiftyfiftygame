1f62f7844c1bda16aff8e1d792c2493db3da5e5c
Bleidhu <ichigosr2014@gmail.com>
2022-06-24 00:16:30 +0200

    added CORS support

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const fs =  require('fs');
+const fs = require('fs');
 const quotes = require('./quotes.json');
+const cors = require('cors')
 let HIGH_SCORE = 0;
 
-fs.readFile('./highScore.txt', function(err, data){
-    if(err)console.log(err);
+fs.readFile('./highScore.txt', function (err, data) {
+    if (err) console.log(err);
     console.log(data);
 
     HIGH_SCORE = parseInt(data.toString());
 
 })
 
-async function updateHs(newHS = 0)
-{
+async function updateHs(newHS = 0) {
     HIGH_SCORE = newHS;
-    fs.writeFile('./highScore.txt', newHS.toString(), function(error){
-        if(error)console.log(error);
+    fs.writeFile('./highScore.txt', newHS.toString(), function (error) {
+        if (error) console.log(error);
     });
 }
 
@@ -24,15 +24,16 @@ const PORT = process.env.PORT ?? 3000;
 
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.json());
 
 app.get('/quote', (req, res) => {
-    console.log(Math.random()%quotes.grzes.length);
-    
-    if(Math.random() > 0.5)
-        res.send(quotes.grzes[Math.floor(Math.random()*quotes.grzes.length)]);
-    else 
-        res.send(quotes.ah[Math.floor(Math.random()*quotes.ah.length)])
+    console.log(Math.random() % quotes.grzes.length);
+
+    if (Math.random() > 0.5)
+        res.send(quotes.grzes[Math.floor(Math.random() * quotes.grzes.length)]);
+    else
+        res.send(quotes.ah[Math.floor(Math.random() * quotes.ah.length)])
 });
 
 app.get('/highScore', (req, res) => {
@@ -40,11 +41,10 @@ app.get('/highScore', (req, res) => {
 });
 
 app.put('/updateHS', (req, res) => {
-    if(parseInt(req.body.score) > HIGH_SCORE)
-    {
+    if (parseInt(req.body.score) > HIGH_SCORE) {
         updateHs(req.body.score).then(res.sendStatus(200));
     }
-    else{ 
+    else {
         res.statusMessage = "too low";
         res.append("reason", "Too low amount");
         res.sendStatus(400);
@@ -54,23 +54,21 @@ app.put('/updateHS', (req, res) => {
 });
 
 app.post('/validateQuote', (req, res) => {
-    
+
     console.log(req.body)
 
-    if(!req.body?.selection || !req.body?.quote)
-    {
+    if (!req.body?.selection || !req.body?.quote) {
         res.sendStatus(400);
         return;
     }
     const selection = req.body.selection;
     const quote = req.body.quote;
 
-    if((selection == "g" && quotes.grzes.includes(quote)) | (selection == "a" && quotes.ah.includes(quote)))
-    {
+    if ((selection == "g" && quotes.grzes.includes(quote)) | (selection == "a" && quotes.ah.includes(quote))) {
         //res.append("verdict", "correct");
-        res.status(200).json({verdict: "correct"});
-    } else{
-        res.status(200).json({verdict: "incorrect"});
+        res.status(200).json({ verdict: "correct" });
+    } else {
+        res.status(200).json({ verdict: "incorrect" });
     }
 });
 
